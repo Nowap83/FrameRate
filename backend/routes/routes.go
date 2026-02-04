599fd9d3ed6e1f996575319aae2f091ac77d6e96
Alexandre Brozzu <alexandrebrozzu@gmail.com>
2026-02-04 09:16:40 +0100

    feat: add of auth/login and auth/register routes #7

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,45 +1,21 @@
 package routes
 
 import (
-    "github.com/Nowap83/FrameRate/backend/config"
     "github.com/Nowap83/FrameRate/backend/middleware"
+		"github.com/Nowap83/FrameRate/backend/handlers"
 		"net/http"
-
+		"gorm.io/gorm"
     "github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(r *gin.Engine) {
+func SetupRoutes(r *gin.Engine, db *gorm.DB) {
+
+		authHandler := handlers.NewAuthHandler(db)	
+
     // Health check (verif serveur)
     r.GET("/health", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{
             "status": "healthy",
-        })
-    })
-
-    // Ping (verif co DB)
-    r.GET("/ping", func(c *gin.Context) {
-        sqlDB, err := config.DB.DB()
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{
-                "message":  "pong",
-                "database": "error",
-                "error":    err.Error(),
-            })
-            return
-        }
-
-        if err := sqlDB.Ping(); err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{
-                "message":  "pong",
-                "database": "disconnected",
-                "error":    err.Error(),
-            })
-            return
-        }
-
-        c.JSON(http.StatusOK, gin.H{
-            "message":  "pong",
-            "database": "connected",
         })
     })
 
@@ -49,19 +25,8 @@ func SetupRoutes(r *gin.Engine) {
         // Auth (publiques)
         auth := api.Group("/auth")
         {
-            // ROUTE TEMPO DE TEST A SUPPR APRES AVOIR CODE REGISTER/LOGIN
-            auth.GET("/generate-test-token", func(c *gin.Context) {
-                token, err := middleware.GenerateToken(2) 
-                if err != nil {
-                    c.JSON(500, gin.H{"error": "Failed to generate token"})
-                    return
-                }
-                c.JSON(200, gin.H{
-                    "token": token,
-                    "message": "test token !",
-                    "user_id": 2,
-                })
-            })
+            auth.POST("/register", authHandler.Register)
+            auth.POST("/login", authHandler.Login)
 
         }
 
