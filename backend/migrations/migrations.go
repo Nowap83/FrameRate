0197ae1ed4c222f78eabb15544fad90493b78e4d
Nowap83 <alexandrebrozzu@gmail.com>
2026-02-08 13:10:02 +0100

    feat: adding all models to AutoMigrate Closes #11

--- backend/migrations/migrations.go
+++ backend/migrations/migrations.go
@@ -1,22 +1,35 @@
 package migrations
 
 import (
-    "github.com/Nowap83/FrameRate/backend/config"
-    "github.com/Nowap83/FrameRate/backend/models"
-    "log"
+	"log"
+
+	"github.com/Nowap83/FrameRate/backend/config"
+	"github.com/Nowap83/FrameRate/backend/models"
 )
 
 // migre auto tout les models
 func AutoMigrateAll() {
-    log.Println("Running database migrations...")
+	log.Println("Running database migrations...")
 
-    err := config.DB.AutoMigrate(
-        &models.User{},
-    )
+	err := config.DB.AutoMigrate(
+		&models.User{},
 
-    if err != nil {
-        log.Fatal("Migration failed:", err)
-    }
+		// Movie models
+		&models.Movie{},
+		&models.Genre{},
+		&models.Country{},
+		&models.Person{},
 
-    log.Println("Database migrated successfully")
+		// Junction tables
+		&models.MovieCast{},
+		&models.MovieCrew{},
+		&models.Track{},
+		&models.Rate{},
+	)
+
+	if err != nil {
+		log.Fatal("Migration failed:", err)
+	}
+
+	log.Println("Database migrated successfully")
 }
